d621c21d5916b9671f30fc8e3a8cf201600932d4
Prem-Thakur786 <Pkpremchand786@gmail.com>
2024-10-17 07:24:48 +0500

    Update

--- Python.py
+++ Python.py
@@ -1,14 +1,1 @@
-#pip install eel 
-import eel
-
-eel.init('D:\prem\HTML')   # Folder Name
-
-@eel.expose
-def App():  #App Main Function
-    print("Application Running")
-
-App()
-
-eel.start('index.html', size=(800,800)):
- # index.html is the main html file
 
